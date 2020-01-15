2690ed748e02523f84923e91f896f9fb103a1fe3
hayley <hayleyfboyce@hotmail.com>
2020-01-14 16:12:37 -0800

    adding requirements

--- exercises/exc_01_01.py
+++ exercises/exc_01_01.py
@@ -1,14 +1,21 @@
 import numpy as np
 import pandas as pd
-dataframe = pd.read_csv('exercises/candybars.csv', header=0, index_col=0)
 
-print(candy_df.head())
+import os
+cwd = os.getcwd()
+new =os.chdir("exercises/")
+print(os.getcwd())
+print(os.listdir(new))
 
-candybar_feat = list(candy_df.columns)
-print(candybar_feat)
+#dataframe = pd.read_csv('exercises/candybars.csv', header=0, index_col=0)
 
-candybar_names = list(candy_df.index)
-print(candybar_names)
+#print(candy_df.head())
 
-candybar_dim = candy_df.shape
-print(candybar_dim)
+#candybar_feat = list(candy_df.columns)
+#print(candybar_feat)
+
+#candybar_names = list(candy_df.index)
+#print(candybar_names)
+
+#candybar_dim = candy_df.shape
+#print(candybar_dim)
