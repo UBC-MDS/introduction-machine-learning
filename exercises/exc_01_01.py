1fa65af7d1a871cc10fa489521b6cd98217062c2
hayley <hayleyfboyce@hotmail.com>
2020-01-14 17:37:51 -0800

    exercise coding workinggit add .

--- exercises/exc_01_01.py
+++ exercises/exc_01_01.py
@@ -1,21 +1,16 @@
-#import numpy as np
+import numpy as np
 import pandas as pd
 
-import os
-cwd = os.getcwd()
-new =os.chdir("exercises/")
-print(os.getcwd())
-print(os.listdir(new))
 
-#dataframe = pd.read_csv('exercises/candybars.csv', header=0, index_col=0)
+dataframe = pd.read_csv('data/candybars.csv', header=0, index_col=0)
 
-#print(candy_df.head())
+print(candy_df.head())
 
-#candybar_feat = list(candy_df.columns)
-#print(candybar_feat)
+candybar_feat = list(candy_df.columns)
+print(candybar_feat)
 
-#candybar_names = list(candy_df.index)
-#print(candybar_names)
+candybar_names = list(candy_df.index)
+print(candybar_names)
 
-#candybar_dim = candy_df.shape
-#print(candybar_dim)
+candybar_dim = candy_df.shape
+print(candybar_dim)
