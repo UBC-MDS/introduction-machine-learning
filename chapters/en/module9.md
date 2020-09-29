eb7d6f6001e2ac5f68bc6a2964dd92a0d9fa5ffd
hayley <hayleyfboyce@hotmail.com>
2020-09-28 17:41:20 -0700

    initial commit for new framework

---
+++ chapters/en/module9.md
@@ -0,0 +1,16 @@
+---
+title: 'Module Closing Remarks'
+description:
+  'Well done on finishing Introduction to Machine Learning'
+prev: /module8
+next: null
+type: chapter
+id: 9
+---
+
+<exercise id="0" title="Congratulations!" type="slides, video">
+
+<slides source="module9/module9_00" shot="13" start="0:005" end="1:57">
+</slides>
+
+</exercise> 
---
+++ src/components/home.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import { graphql, StaticQuery } from 'gatsby'
+
+import Layout from './layout'
+import { Link } from './link'
+import Logo from '../../static/logo.svg'
+
+import classes from '../styles/index.module.sass'
+
+export default ({ lang = 'en' }) => {
+    return (
+        <StaticQuery
+            query={query}
+            render={data => {
+                const chapters = data.allMarkdownRemark.edges
+                    .filter(({ node }) => node.fields.lang === lang)
+                    .map(({ node }) => ({
+                        slug: node.fields.slug,
+                        title: node.frontmatter.title,
+                        description: node.frontmatter.description,
+                    }))
+                return (
+                    <Layout isHome lang={lang}>
+                        <Logo className={classes.logo} />
+                        <section>
+                                <h1 className={classes.subtitle}><center>Programming in Python for Data Science</center></h1>
+                            <div className={classes.introduction}>
+                                
+                                        <p>
+                                        <center>
+                                        Welcome to Introduction to Machine Learning!
+                                        This course is part of the  <u><strong><a href="https://extendedlearning.ubc.ca/programs/key-capabilities-data-science" target="_blank" >Key Capabilities for Data Science program</a></strong></u> and will teach you how to conduct data analysis in Python.
+                                        <br></br>During this course ...
+                                        TBD
+                                        </center>
+                                        </p>
+                                        <p>
+                                        <strong>Course prerequisites:</strong>  <u><a href="https://prog-learn.mds.ubc.ca/" target="_blank" >Programming in Python for Data Science</a></u> 
+                        </p>
+                             </div>
+                    </section>
+                        {chapters.map(({ slug, title, description }) => (
+                            <section key={slug} className={classes.chapter}>
+                                <h2 className={classes.chapterTitle}>
+                                    <Link hidden to={slug}>
+                                        {title}
+                                    </Link>
+                                </h2>
+                                <p className={classes.chapterDesc}>
+                                    <Link hidden to={slug}>
+                                        {description}
+                                    </Link>
+                                </p>
+                            </section>
+                        ))}
+                    </Layout>
+                )
+            }}
+        />
+    )
+}
+
+const query = graphql`
+    {
+        allMarkdownRemark(
+            sort: { fields: [frontmatter___title], order: ASC }
+            filter: { frontmatter: { type: { eq: "chapter" } } }
+        ) {
+            edges {
+                node {
+                    fields {
+                        slug
+                        lang
+                    }
+                    frontmatter {
+                        title
+                        description
+                    }
+                }
+            }
+        }
+    }
+`
