dd02abaa538a651b76fd9c3449db0362badde89c
Hayley Boyce <42727648+hfboyce@users.noreply.github.com>
2020-11-01 19:21:37 -0800

    Update test_03_12.py

--- exercises/en/test_03_12.py
+++ exercises/en/test_03_12.py
@@ -8,7 +8,7 @@ def test():
     assert 'DecisionTreeClassifier' in __solution__, "Make sure you are specifying a 'DecisionTreeClassifier'."
     assert "return_train_score" in __solution__, "Make sure you are retaining the training score using the 'return_train_score' function."
     assert 'cross_validate' in __solution__, "Make sure you are doing cross validation using the 'cross_validate' function."
-    assert 'cv=10' in __solution__, "Make sure you are doing 6-fold cross validation."
+    assert 'cv=10' in __solution__, "Make sure you are doing 10-fold cross validation."
     assert '.mean()' in __solution__, "Make sure you are taking the mean of each column?"
     assert round(mean_scores.values[2],2) - 0.96 < 0.00001, "The mean test score is incorrect. Are you fitting the model correctly?"
     assert round(mean_scores.values[3],2) - 0.99 < 0.00001, "The mean training score is incorrect. Are you fitting the model correctly?"
