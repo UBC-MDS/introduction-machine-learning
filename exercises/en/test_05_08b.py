bead6da541a072c0bb7a9bed77a5356531479ed8
ecool50 <ewillie@sfu.ca>
2020-11-16 19:28:27 -0800

    Update test_05_08b.py

--- exercises/en/test_05_08b.py
+++ exercises/en/test_05_08b.py
@@ -10,6 +10,6 @@ def test():
     assert "imputer.fit" in __solution__,  "Make sure you are fitting the imputation model on the test data."
     assert isinstance(X_train_imp_df,pd.DataFrame), "Make sure that X_train_imp_df is a dataframe."
     assert sorted(list(X_train_imp_df.columns)) == ['height', 'salary', 'weight'], "Your dataframe contains incorrect columns. Are you selecting the correct columns?"
-    assert __solution__.count('imputer.transform') == 2, "Make sure you are transforming both the training and the test data."
+    assert __solution__.count('imputer.transform') >= 1, "Make sure you are transforming both the training and the test data."
     assert X_train_imp_df.isnull().any(axis=1).sum() == 0, "There is missing data in your dataframe. Are you imputing properly?"
     __msg__.good("Nice work, well done!")
