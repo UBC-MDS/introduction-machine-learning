6ed12c4e203d73be0889db98cf2fd1da92072198
Elijah Willie <ewillie@sfu.ca>
2020-11-16 19:24:00 -0800

    Made A5 content amendments

--- exercises/en/test_05_20.py
+++ exercises/en/test_05_20.py
@@ -6,10 +6,13 @@ def test():
 
     # If an assertion fails, the message will be displayed
     assert '"knn__n_neighbors"' in __solution__, "Make sure you are setting the knn grid parameter properly."
-    assert '"knn__weights"' in __solution__, "Make sure you are weighting knn using the 'uniform' and 'distance' metrics."
     assert "GridSearchCV" in __solution__,  "Make sure you are calling the GridSearchCV() function on your param_grid and bb_pipe."
+    assert '"knn__weights"' in __solution__, "Make sure you are weighting knn using the 'uniform' and 'distance' metrics."
     assert "cv=10" in __solution__, "Make sure you are using 10-fold cross validation."
     assert "grid_search.fit" in __solution__, "Make sure you are fitting the grid model on the training data."
     assert "grid_search.score" in __solution__, "Make sure you are scoring the model on the test set."
     assert round(bb_test_score,3) == 0.919, "Your model score is incorrect. Are you scoring the model on the test set?"
+    assert 'best_params_' in __solution__, "Make sure you are extracting the best parameters from the grid search."
+    assert best_hyperparams['knn__n_neighbors'] == 40, "The value for number of neighbors is incorrect. Are you setting up the grid properly?"
+    assert best_hyperparams['knn__weights'] == 'uniform', "The value for knn__weights is incorrect. Are you setting up the grid properly?"
     __msg__.good("Nice work, well done!")
